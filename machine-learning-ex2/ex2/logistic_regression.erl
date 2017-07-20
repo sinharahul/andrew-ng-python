27718fce67719e5e68f88abb72c52c79ab47239b
sinharahul <sinharahul@gmail.com>
2017-07-20 00:13:07 -0400

    Added matrix  operations https://www.ugrad.cs.ubc.ca/~cs418/2013-1/lecture/09-24.pdf

--- machine-learning-ex2/ex2/logistic_regression.erl
+++ machine-learning-ex2/ex2/logistic_regression.erl
@@ -4,6 +4,50 @@ load(File)->
 	 {ResultCode, InputFile} = file:open(File, [read]),
      IntegerList=parse(InputFile,[]).
 	 
+addOnes(X)->
+	[[1,A,B]|| [A,B,_] <- X].
+		 
+y(X) ->
+	[C|| [_,_,C] <- X].	
+	
+size(X)->
+	L=length(X),
+	A=lists:nth(1,X), 
+	{L,length(A)}.
+		
+zeros(N)->
+	zeros(N,[]).
+zeros(N,L) when N >=1 ->
+	zeros(N-1,lists:append(0,L));
+zeros(0,L)->
+	L.
+%%https://www.ugrad.cs.ubc.ca/~cs418/2013-1/lecture/09-24.pdf	
+transpose([]) -> []; 
+transpose([[]|_]) -> []; 
+transpose(M) ->
+    [ [H || [H | T] <- M ] 
+      | transpose([ T || [ H | T] <- M ]) 
+    ].
+	
+dot_prod(V1, V2) ->
+lists:foldl(
+fun({X,Y},Sum) ->
+	  Sum + X*Y end,
+      0, lists:zip(V1, V2)).
+
+dotProd([], []) -> 0;
+dotProd([A | Atl], [B | Btl]) -> A*B + dotProd(Atl, Btl).
+
+mult(A, B) ->
+  BT = transpose(B),
+  lists:map(
+    fun(Row_of_A) ->
+       lists:map(
+           fun(Col_of_B) ->
+              dot_prod(Row_of_A, Col_of_B)
+           end, BT)
+    end, A).
+					
 parse(InputFile,List)->
       case io:get_line(InputFile,"") of
 	    eof -> file:close(InputFile),
